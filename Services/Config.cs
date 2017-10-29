53dec82025b1baa441acd38abafebbe44c96ac77
kckct <ss010359@gmail.com>
2017-10-30 00:25:14 +0800

    加入Config.cs & ConfigManager.cs

---
+++ Services/Config.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Services
+{
+    /// <summary>
+    /// Config 設定
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 資料庫連接字串
+        /// </summary>
+        public string ConnectionString { get; }
+
+        /// <summary>
+        /// 儲存目的地
+        /// </summary>
+        public string Destination { get; }
+
+        /// <summary>
+        /// 處理後的目錄
+        /// </summary>
+        public string Dir { get; }
+
+        /// <summary>
+        /// 檔案格式
+        /// </summary>
+        public string Ext { get; }
+
+        /// <summary>
+        /// 處理方式
+        /// </summary>
+        public string Handler { get; }
+
+        /// <summary>
+        /// 備份檔案的目錄
+        /// </summary>
+        public string Location { get; }
+
+        /// <summary>
+        /// 處理完是否刪除檔案
+        /// </summary>
+        public bool Remove { get; }
+
+        /// <summary>
+        /// 是否處理子目錄
+        /// </summary>
+        public bool SubDirectory { get; }
+
+        /// <summary>
+        /// 備份單位
+        /// </summary>
+        public string Unit { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="config">設定物件</param>
+        public Config(JToken config)
+        {
+            ConnectionString = (string)config["connectionString"];
+            Destination = (string)config["destination"];
+            Dir = (string)config["dir"];
+            Ext = (string)config["ext"];
+            Handler = (string)config["handler"];
+            Location = (string)config["location"];
+            Remove = (bool)(config["remove"] ?? "false");
+            SubDirectory = (bool)(config["subDirectory"] ?? "false");
+            Unit = (string)config["unit"];
+        }
+    }
+}
---
+++ Services/ConfigManager.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Services
+{
+    /// <summary>
+    /// Config 管理
+    /// </summary>
+    public class ConfigManager
+    {
+        /// <summary>
+        /// config.json 檔案路徑
+        /// </summary>
+        const string FILEPATH = @"D:\\Projects\\oop-homework-dotnet-core\\Services\\configs\\config.json";
+
+        /// <summary>
+        /// Config List
+        /// </summary>
+        private List<Config> configs = new List<Config>();
+
+        /// <summary>
+        /// 以 index 方式讀取物件屬性
+        /// </summary>
+        /// <param name="index">index</param>
+        /// <returns>configs[index]</returns>
+        public Config this[int index] => configs[index];
+
+        /// <summary>
+        /// 筆數
+        /// </summary>
+        public int Count => configs.Count;
+
+        /// <summary>
+        /// 處理 config.json 設定檔
+        /// </summary>
+        public void ProcessConfigs()
+        {
+            // 讀取 json 檔
+            string json = File.ReadAllText(FILEPATH);
+
+            // json parse
+            JObject obj = JObject.Parse(json);
+
+            // 整理成 Config 放到 configs
+            foreach (JToken config in obj["configs"])
+            {
+                configs.Add(new Config(config));
+            }
+        }
+    }
+}
