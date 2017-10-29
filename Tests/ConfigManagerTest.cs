52ddf1fcdbe92b10a1bfccd794541a6d8c737d19
kckct <ss010359@gmail.com>
2017-10-30 00:25:56 +0800

    加入ConfigTest & ConfigManagerTest 測試

---
+++ Tests/ConfigManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using Services;
+using Xunit;
+
+namespace Tests
+{
+    /// <summary>
+    /// ConfigManager 測試
+    /// </summary>
+    public class ConfigManagerTest
+    {
+        [Fact]
+        public void 讀取設定檔後屬性型態及筆數正確()
+        {
+            // act
+            ConfigManager configManager = new ConfigManager();
+            configManager.ProcessConfigs();
+
+            // assert
+            // 是否為 Config 型態
+            Assert.IsType<Config>(configManager[0]);
+            // 是否為 3 筆
+            Assert.Equal(3, configManager.Count);
+        }
+    }
+}
