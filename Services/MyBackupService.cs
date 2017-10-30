f07f1086034f5a83ce621d19a8d67a1cb8ab494c
kckct <ss010359@gmail.com>
2017-10-30 14:34:24 +0800

    加入 MyBackupService.cs

---
+++ Services/MyBackupService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Services
+{
+    /// <summary>
+    /// MyBackupService
+    /// </summary>
+    public class MyBackupService
+    {
+        /// <summary>
+        /// JsonManager List
+        /// </summary>
+        private readonly List<JsonManager> managers = new List<JsonManager>();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MyBackupService()
+        {
+            managers.Add(new ConfigManager());
+            managers.Add(new ScheduleManager());
+        }
+
+        /// <summary>
+        /// 處理 json 設定檔
+        /// </summary>
+        public void ProcessJsonConfigs()
+        {
+            foreach (JsonManager manager in managers)
+            {
+                manager.ProcessJsonConfig();
+            }
+        }
+    }
+}
