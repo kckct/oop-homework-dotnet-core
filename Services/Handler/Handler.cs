4f2d5674b02441a426a28a8829f03878cc98af05
kckct <ss010359@gmail.com>
2017-11-09 22:08:46 +0800

    加入 interface Handler

---
+++ Services/Handler/Handler.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Handler
+{
+    /// <summary>
+    /// Handler interface
+    /// </summary>
+    public interface Handler
+    {
+        /// <summary>
+        /// 處理檔案
+        /// </summary>
+        /// <param name="candidate">描述待處理檔案的資訊</param>
+        /// <param name="target">byte[]</param>
+        /// <returns>byte[]</returns>
+        byte[] Perform(Candidate candidate, byte[] target);
+    }
+}
