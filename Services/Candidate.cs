df0eb9fde8f3eb5830dbfcc1681f2ea637a1b615
kckct <ss010359@gmail.com>
2017-11-09 22:40:48 +0800

    加入 Candidate 物件

---
+++ Services/Candidate.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Services
+{
+    public class Candidate
+    {
+        /// <summary>
+        /// Config 物件
+        /// </summary>
+        public Config Config { get; }
+
+        /// <summary>
+        /// 檔案的日期與時間
+        /// </summary>
+        public string FileDateTime { get; }
+
+        /// <summary>
+        /// 檔案名稱
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// 處理檔案的 process
+        /// </summary>
+        public string ProcessName { get; }
+
+        /// <summary>
+        /// 檔案 size
+        /// </summary>
+        public string Size { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="candidate">設定物件</param>
+        public Candidate(JToken candidate)
+        {
+            Config = candidate["config"].ToObject<Config>();
+            FileDateTime = (string)candidate["fileDateTime"];
+            Name = (string)candidate["name"];
+            ProcessName = (string)candidate["processName"];
+            Size = (string)candidate["size"];
+        }
+    }
+}
