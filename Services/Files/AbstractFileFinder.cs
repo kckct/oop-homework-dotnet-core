7704b239580dd063a09470667f3c2a608f0edcf5
kckct <ss010359@gmail.com>
2017-11-13 01:37:21 +0800

    tmp

---
+++ Services/Files/AbstractFileFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace Services.Files
+{
+    class AbstractFileFinder : FileFinder
+    {
+        protected Config config;
+        protected string[] files;
+        protected int index = -1;
+
+        protected AbstractFileFinder()
+        {
+        }
+
+        protected AbstractFileFinder(Config config)
+        {
+            if (config != null)
+                this.config = config;
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public object Current => files[index];
+
+        public bool MoveNext()
+        {
+            index++;
+            return (index < files.Length);
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        protected abstract Candidate CreateCandidate(string fileName);
+    }
+}
---
+++ Services/Files/LocalFileFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Services.Files
+{
+    class LocalFileFinder : AbstractFileFinder
+    {
+        public LocalFileFinder()
+        {
+        }
+
+        public LocalFileFinder(Config config) : base(config)
+        {
+            // 是否處理子目錄
+            if (config.SubDirectory)
+            {
+                // 取得所有子目錄所有的檔名
+                files = GetSubDirectoryFiles(config);
+            }
+            else
+            {
+                // 取得此目錄所有的檔名
+                files = Directory.GetFiles(config.Location, "*." + config.Ext);
+            }
+        }
+
+        private string[] GetSubDirectoryFiles(Config config)
+        {
+            List<string> allFiles = new List<string>();
+            string[] subdirectoryEntries = Directory.GetDirectories(config.Location);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                allFiles.Add(Directory.GetFiles(subdirectory, "*." + config.Ext));
+            }
+        }
+
+        protected override Candidate CreateCandidate(string fileName)
+        {
+
+        }
+    }
+}
