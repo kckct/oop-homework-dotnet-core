ae172abbaea3e61dcf447c9a28dd7a7f5ac8f0ef
kckct <ss010359@gmail.com>
2017-11-12 23:58:50 +0800

    加入 DirectoryHandler & 測試

---
+++ Services/Handlers/DirectoryHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace Services.Handlers
+{
+    /// <summary>
+    /// DirectoryHandler 檔案複製
+    /// </summary>
+    public class DirectoryHandler : AbstractHandler
+    {
+        /// <summary>
+        /// 處理檔案
+        /// </summary>
+        /// <param name="candidate">描述待處理檔案的資訊</param>
+        /// <param name="target">byte[]</param>
+        /// <returns>byte[]</returns>
+        public override byte[] Perform(Candidate candidate, byte[] target)
+        {
+            base.Perform(candidate, target);
+
+            // byte[] 為空時 直接回傳
+            if (target == null)
+            {
+                return target;
+            }
+
+            // 將 byte[] 還原檔案並複製到其他目錄
+            CopyToDirectory(candidate, target);
+
+            return target;
+        }
+
+        /// <summary>
+        /// 還原檔案並複製到其他目錄
+        /// </summary>
+        /// <param name="candidate">描述待處理檔案的資訊</param>
+        /// <param name="target">byte[]</param>
+        private void CopyToDirectory(Candidate candidate, byte[] target)
+        {
+            // 還原檔案
+            FileHandler fileHandler = new FileHandler();
+            fileHandler.Perform(candidate, target);
+
+            // 原檔案路徑
+            string oldFilePath = candidate.Name + ".backup";
+
+            // 檔名
+            string backupFileName = Path.GetFileName(oldFilePath);
+
+            // config.json 內所設定的 dir 目錄
+            string newDir = candidate.Config.Dir;
+
+            // 複製後的檔案路徑
+            string newFilePath = newDir + Path.DirectorySeparatorChar + backupFileName;
+
+            // 複製檔案
+            File.Copy(oldFilePath, newFilePath);
+        }
+    }
+}
---
+++ Tests/DirectoryHandlerTest.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using Services;
+using Services.Handlers;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Tests
+{
+    /// <summary>
+    /// DirectoryHandler 測試
+    /// </summary>
+    public class DirectoryHandlerTest
+    {
+        private DirectoryHandler directoryHandler;
+
+        public DirectoryHandlerTest()
+        {
+            directoryHandler = new DirectoryHandler();
+        }
+
+        [Fact]
+        public void Test_將byte陣列還原成檔案並複製到其他目錄_傳入target有值_應複製檔案()
+        {
+            // arrange
+            // 測試執行時預期產生的檔案
+            string byteArrayToFile = "D:\\Projects\\oop-homework\\storage\\app\\test.txt.backup";
+            // 測試完預期產生的檔案
+            string copyToNewFile = "D:\\Projects\\oop-homework\\storage\\app\\backup\\test.txt.backup";
+
+            // 產生假 Candidate 物件
+            Candidate candidateStub = CreateFakeCandidate();
+            byte[] targetStub = new byte[1];
+
+            // act
+            byte[] actual = directoryHandler.Perform(candidateStub, targetStub);
+
+            // assert
+            // 查看是否有檔案產生
+            Assert.True(File.Exists(byteArrayToFile));
+            Assert.True(File.Exists(copyToNewFile));
+
+            // 測試結束刪除檔案
+            File.Delete(byteArrayToFile);
+            File.Delete(copyToNewFile);
+            Assert.False(File.Exists(byteArrayToFile));
+            Assert.False(File.Exists(copyToNewFile));
+        }
+
+        [Fact]
+        public void Test_將byte陣列還原成檔案並複製到其他目錄_傳入target為空陣列_應回傳空陣列()
+        {
+            // arrange
+            // 產生假 Candidate 物件
+            Candidate candidateStub = CreateFakeCandidate();
+            byte[] targetStub = null;
+
+            // act
+            byte[] actual = directoryHandler.Perform(candidateStub, targetStub);
+
+            // assert
+            Assert.Null(actual);
+        }
+
+        /// <summary>
+        /// 產生假 Candidate 物件
+        /// </summary>
+        /// <returns>Candidate 物件</returns>
+        private Candidate CreateFakeCandidate()
+        {
+            JObject inputStub = JObject.Parse(@"{'configs':[{'connectionString':'','destination':'directory','dir':'D:\\Projects\\oop-homework\\storage\\app\\backup','ext':'txt','handlers':['zip', 'encode'],'location':'D:\\Projects\\oop-homework\\storage\\app','remove':false,'subDirectory':true,'unit':'file'}]}");
+
+            Config configStub = new Config(inputStub["configs"][0]);
+            Candidate candidateStub = new Candidate(
+                configStub,
+                "2017-11-12 12:34:56",
+                "D:\\Projects\\oop-homework\\storage\\app\\test.txt",
+                "xxx",
+                "123"
+            );
+
+            return candidateStub;
+        }
+    }
+}
