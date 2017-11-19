b7778de80317aa61152481b8b7fe58d2dfb044c8
kckct <ss010359@gmail.com>
2017-11-20 00:06:14 +0800

    加入 TaskFactory

---
+++ Services/Tasks/TaskFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Services.Tasks
+{
+    /// <summary>
+    /// TaskFactory Task 工廠
+    /// </summary>
+    public static class TaskFactory
+    {
+        /// <summary>
+        /// 建立對應的 Task 物件
+        /// </summary>
+        /// <param name="task">task key</param>
+        /// <returns>Task 物件</returns>
+        public static Task Create(string task)
+        {
+            if (task == "simple")
+            {
+                return new SimpleTask();
+            }
+            else if (task == "scheduled")
+            {
+                return new ScheduledTask();
+            }
+
+            return null;
+        }
+    }
+}
