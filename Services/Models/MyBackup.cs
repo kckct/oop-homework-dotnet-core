4bd31cfca4e4c89d1492bd0502287da8e4d3a1ed
kckct <ss010359@gmail.com>
2017-11-23 23:50:20 +0800

    加入 MyBackup & MyLog model

---
+++ Services/Models/MyBackup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services.Models
+{
+    /// <summary>
+    /// MyBackup model
+    /// </summary>
+    public class MyBackup
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 檔名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 檔案時間
+        /// </summary>
+        public DateTime FileDateTime { get; set; }
+
+        /// <summary>
+        /// 檔案大小
+        /// </summary>
+        public long Size { get; set; }
+
+        /// <summary>
+        /// 資料處理
+        /// </summary>
+        public List<string> Handlers { get; set; }
+
+        /// <summary>
+        /// 檔案內容
+        /// </summary>
+        public Byte[] Target { get; set; }
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Services/Models/MyLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Services.Models
+{
+    /// <summary>
+    /// MyLog model
+    /// </summary>
+    public class MyLog
+    {
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 檔名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Config 物件
+        /// </summary>
+        public Config Config { get; set; }
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
