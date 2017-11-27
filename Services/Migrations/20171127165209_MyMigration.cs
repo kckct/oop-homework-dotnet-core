95a7bb5ccaf03545274a3849320ce53409cc375b
kckct <ss010359@gmail.com>
2017-11-28 01:05:44 +0800

    dotnet ef migrations add MyMigration

---
+++ Services/Migrations/20171127165209_MyMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Services.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyBackup",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    FileDateTime = table.Column<DateTime>(nullable: false),
+                    Handlers = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Size = table.Column<long>(nullable: false),
+                    Target = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyBackup", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MyLog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ConnectionString = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    Destination = table.Column<string>(nullable: true),
+                    Dir = table.Column<string>(nullable: true),
+                    Ext = table.Column<string>(nullable: true),
+                    Handlers = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Remove = table.Column<bool>(nullable: false),
+                    SubDirectory = table.Column<bool>(nullable: false),
+                    Unit = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyLog", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyBackup");
+
+            migrationBuilder.DropTable(
+                name: "MyLog");
+        }
+    }
+}
