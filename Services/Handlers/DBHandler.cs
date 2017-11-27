607518bd787f3e802b03c13053861896a4ff914f
kckct <ss010359@gmail.com>
2017-11-28 01:11:58 +0800

    加入 DBHandler

---
+++ Services/Handlers/DBHandler.cs
@@ -0,0 +1,18 @@
+﻿using Candidates;
+
+namespace Services.Handlers
+{
+    /// <summary>
+    /// DBHandler interface
+    /// </summary>
+    public interface DBHandler
+    {
+        /// <summary>
+        /// 處理檔案
+        /// </summary>
+        /// <param name="candidate">描述待處理檔案的資訊</param>
+        /// <param name="target">byte[]</param>
+        /// <returns>byte[]</returns>
+        byte[] Perform(Candidate candidate, byte[] target);
+    }
+}
